4fb7c7fcdc880fabaed49a345968425e8ae51b0c
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-14 10:49:48 -0600

    fix build

--- lib/domain.ts
+++ lib/domain.ts
@@ -1,5 +1,5 @@
 const getSubdomain = (host: string) => {
-    const parts = host.split('//')[1].split('.');
+    const parts = host.split("//")[1].split(".");
     if (parts.length === 3) {
         return parts[0];
     } else {
