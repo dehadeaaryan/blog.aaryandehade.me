01fdd6802abacde5209a4966cff2db26e8212dab
dehadeaaryan <dehadeaaryan@gmail.com>
2024-02-14 23:18:05 -0600

    retry failed build

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 import { getSubdomain } from "@/lib/domain";
 
 export default function Home() {
-
+    const subdomain = typeof window !== 'undefined' ? getSubdomain(window.location.href) : '';
     return (
         <div className="w-screen h-screen">
             <h1>Aaryan Dehade&apos;s Blog</h1>
-            <p>Welcome to {getSubdomain(window.location.href)}!</p>
+            <p>Welcome to {subdomain}!</p>
         </div>
     );
 }
